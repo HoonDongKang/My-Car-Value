5dc65dccceb260f97db61eb9745cb068f229c0e1
HoonDongKang <d159123@naver.com>
2023-02-14 21:52:55 +0900

    ENH: Create createUser API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // request 요청 시 필요 params만 가져옴
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
